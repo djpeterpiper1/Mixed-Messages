b30c1596cabea9a5bc4f5bf97d661942970df8ee
Peter Toussaint <dj.peterpiper1@gmail.com>
2021-04-04 00:04:23 -0400

    add message arrays

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict";
 
 
+const love = ["will find a new love", "will lose your love", "are in love"];
+const health = [
+  "will live a long life",
+  "will have a terrible illness",
+  "will die soon",
+];
+const money = ["will be rich", "will lose all your money", "are poor"];
+
